887ff2efd937b2238697eed9c3151da21c919ed7
Kostiantyn S <kostia007@gmail.com>
2019-12-25 18:04:15 +0200

    add styles to header

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -4,9 +4,11 @@ import  style from "./Search.module.css";
 export default class Search extends React.Component {
     render() {
         return (
-            <div className={style.search}>
-                <input type="text" className={style.searchTerm} placeholder="What are you looking for?"/>
-                <button type="submit" className={style.searchButton}>&#128269;</button>
+            <div className={style.search_wrappwer}>
+                <div className={style.search}>
+                    <input type="text" className={style.searchTerm} placeholder="What are you looking for?"/>
+                    <button type="submit" className={style.searchButton}>&#128269;</button>
+                </div>
             </div>
         )
     }
