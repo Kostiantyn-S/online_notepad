92eadb4a4ea58e107487ed439bd1286294e72fa7
Kostiantyn S <kostia007@gmail.com>
2019-12-29 22:41:23 +0200

    Add help window template with styles.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import  style from "./Footer.module.css";
+import style from "./Footer.module.css";
+import Help from "./Help/Help";
 
 export default class Footer extends React.Component {
     constructor(props) {
@@ -9,12 +10,13 @@ export default class Footer extends React.Component {
     render() {
         return (
             <div className={style.footer}>
-                <p>Sort for:</p>
-                <div><div>Day</div></div>
-                <div><div>Week</div></div>
-                <div><div>Month</div></div>
-                <div><div>Year</div></div>
-                <div><div>Don't sort</div></div>
+                <p className={style.sort_description}>Sort for:</p>
+                <div className={style.sort_day}><div>Day</div></div>
+                <div className={style.sort_week}><div>Week</div></div>
+                <div className={style.sort_month}><div>Month</div></div>
+                <div className={style.sort_year}><div>Year</div></div>
+                <div className={style.sort_dont}><div>Don't sort</div></div>
+                <Help/>
             </div>
         )
     }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import  style from "./Main.module.css";
 import MainContent from './MainContent/MainContent';
+import Help_window from "./../Footer/Help_window/Help_window";
 
 export default class Main extends React.Component {
     constructor(props) {
@@ -10,6 +11,7 @@ export default class Main extends React.Component {
     render() {
         return (
             <div className={style.main}>
+                <Help_window/>
             </div>
         )
     }
